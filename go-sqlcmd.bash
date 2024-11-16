081f6afca066b31a971c583957df5e0b2a325110
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-11-16 17:23:17 -0300

    chore: add `--rm` flag to `go-sqlcmd`

--- go-sqlcmd.bash
+++ go-sqlcmd.bash
@@ -10,11 +10,11 @@ DB_NAME_OR_EMPTY=$(if [ -n "${DB_NAME}" ]; then echo "-d ${DB_NAME}"; fi)
 DB_SERVER_URL="${DB_HOST},${DB_PORT}"
 
 docker run \
-    -it \
+    -it --rm \
     --network "${CONTAINER_NETWORK}" \
     --name go-sqlcmd \
     ghcr.io/bonddim/go-sqlcmd \
     -S "${DB_SERVER_URL}" \
     -U "${DB_USER}" \
     -P "${DB_PASS}" \
-    ${DB_NAME_OR_EMPTY} 
+    ${DB_NAME_OR_EMPTY}
