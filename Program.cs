dae318124788d6fa25952604f57f1f8dd93782ac
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-11-16 17:22:50 -0300

    style: apply format to `Program`

--- Program.cs
+++ Program.cs
@@ -1,20 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews()
+builder
+  .Services
+  .AddControllersWithViews()
   .AddRazorOptions(
     options =>
     {
       options.ViewLocationFormats.Add("/src/Views/{1}/{0}.cshtml");
       options.ViewLocationFormats.Add("/src/Views/Shared/{0}.cshtml");
     }
-);
+  );
+
 
 var app = builder.Build();
 
 app.UseRouting();
+
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}"
+  name: "default",
+  pattern: "{controller=Home}/{action=Index}/{id?}"
 );
 
 app.Run();
