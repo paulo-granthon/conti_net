6aea57a070a780b3c65b80e85e39931b33ff6883
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-11-15 22:30:44 -0300

    chore: config `Views` directory

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+  .AddRazorOptions(
+    options =>
+    {
+      options.ViewLocationFormats.Add("/src/Views/{1}/{0}.cshtml");
+      options.ViewLocationFormats.Add("/src/Views/Shared/{0}.cshtml");
+    }
+);
 
 var app = builder.Build();
 
