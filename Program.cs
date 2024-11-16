1c266e43833d00b86feb89f24a1c5c493345f47a
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2024-11-16 17:35:46 -0300

    feat: configure database acces with envinronment

--- Program.cs
+++ Program.cs
@@ -14,6 +14,30 @@ builder
 
 Env.Load();
 
+string GetEnv(string name) =>
+  Environment.GetEnvironmentVariable(name)
+  ?? throw new Exception($"Missing environment variable: {name}");
+
+string DbHost = builder.Environment.IsProduction()
+  ? GetEnv("DB_HOST")
+  : "localhost";
+
+string GenerateDbConnectionString() =>
+  $"Server={DbHost},{GetEnv("DB_PORT")};" +
+  $"Database={GetEnv("DB_NAME")};" +
+  $"User Id={GetEnv("DB_USER")};" +
+  $"Password={GetEnv("DB_PASS")};" +
+  "Encrypt=True;TrustServerCertificate=True";
+
+builder
+  .Services
+  .AddDbContext<ApplicationDbContext>(
+    options => options.UseSqlServer(
+      GenerateDbConnectionString(),
+      builder => builder.MigrationsAssembly("ContiNet")
+    )
+  );
+
 builder.Services.AddScoped<CountryService>();
 builder.Services.AddScoped<ContinentService>();
 
